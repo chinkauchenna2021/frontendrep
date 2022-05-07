b43097b6152dae5c6754ff8df9245cfae7143a20
chinkauchenna2021 <84646759+chinkauchenna2021@users.noreply.github.com>
2022-05-07 00:33:42 -0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -172,18 +172,18 @@ const clicker2 = ()=>{
 <div className="lg:flex lg:items-center lg:justify-between align-middle flex-column justify-center sm:flex">
   <div className="flex justify-center md:w-30 px-10">
        
-//         <Image
-//        className="object-contain md:ml-5 w-3 cursor-pointer"
-//          src="../img/image3.png"
-//          width={140}
-//          height={80}
-//          alt="logo"
-//        />
+        <Image
+       className="object-contain md:ml-5 w-3 cursor-pointer"
+         src="/img/image3.png"
+         width="140px"
+         height="80px"
+         alt="logo"
+       />
        
   </div>
 
 
-  <div className="transition-all  w-8 hover:w-3/4 rounded lg:ml-40 top-40  left-50 absolute   flex  lg:top-0 lg:relative   justify-center lg:justify-start lg:w-3/4  px-auto" style={{backgroundColor:"#1F1F1F"}}>
+  <div className="transition-all  w-8 hover:w-3/4 rounded lg:ml-40 top-40  left-50 absolute   flex  lg:top-0 lg:relative   lg:justify-start lg:w-3/4  px-auto" style={{backgroundColor:"#1F1F1F",justifyContent:"center"}}>
   <svg  className=" h-6 w-6 absolute top-3  text-gray-400  left-2 lg:left-4 lg:top-3" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
   <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
 </svg>
@@ -271,9 +271,9 @@ const clicker2 = ()=>{
 
                <Image
        className="object-contain md:ml-5  mx-auto"
-         src="./img/Ellipse37.png"
-         width={100}
-         height={100}
+         src="/img/Ellipse37.png"
+         width="100px"
+         height="100px"
          alt="body"
        />
                    
@@ -1162,9 +1162,9 @@ const clicker2 = ()=>{
 <div className="">
 <Image
        className="object-contain md:ml-5 w-3"
-         src="./img/image3.png"
-         width={190}
-         height={80}
+         src="/img/image3.png"
+         width="190px"
+         height="80px"
          alt="footer-holder"
        />
        
